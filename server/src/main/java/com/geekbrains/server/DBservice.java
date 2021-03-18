9bd631fa7e92dff36b880f5a85cbc3caf308b3b5
pfrost982 <power982@gmail.com>
2021-03-19 00:15:27 +0700

    Авторизация по базе данных

---
+++ server/src/main/java/com/geekbrains/server/DBservice.java
@@ -0,0 +1,70 @@
+package com.geekbrains.server;
+
+import java.sql.*;
+
+public class DBservice {
+    private static DBservice dbService;
+
+    private Connection connection = null;
+    private Statement statement = null;
+
+    private DBservice() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:auth_list.db");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static DBservice getDBservice() {
+        if (dbService == null) {
+            dbService = new DBservice();
+        }
+        return dbService;
+    }
+
+    public String getNicknameByLoginAndPassword(String login, String password) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM auth_list WHERE login = ? AND password = ?");
+            preparedStatement.setString(1, login);
+            preparedStatement.setString(2, password);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                String nickname = resultSet.getString("nickname");
+                return nickname;
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void printTable() {
+        ResultSet resultSet = null;
+        try {
+            resultSet = statement.executeQuery("SELECT * FROM auth_list");
+            while (resultSet.next()) {
+                String login = resultSet.getString("login");
+                String password = resultSet.getString("password");
+                String nickname = resultSet.getString("nickname");
+
+                System.out.println(login + " " + password + " " + nickname);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
